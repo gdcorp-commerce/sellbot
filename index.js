8d4796bdf1adee94afddbf5a7ea5dd0262a3bc8a
Charles Feng <c@poynt.co>
2020-09-26 20:31:02 -0500

    global configs. poynt collect

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "pug");
+app.use(bodyParser.json());
 app.use(express.static("public"));
+
+// global configs
+global.configs = require("./lib/configs");
 
 // redirect home to Connect demo
 app.get("/", (_req, res) => {
   res.redirect("/connect");
 });
 
-// /connect is a demo of Connect wth Poynt
 app.use("/connect", require("./routes/connect"));
+app.use("/collect", require("./routes/collect"));
 
 const port = 1347;
 app.listen(port, () => {
---
+++ routes/collect.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+router.get("/", (_req, res) => {
+  res.redirect("/collect/tokenize");
+});
+
+router.get("/tokenize", (req, res) => {
+  res.render("collect/tokenize", {
+    apiKey: req.query.apiKey || global.configs.collectApiKey,
+    applicationId: global.configs.applicationId,
+    businessId: req.query.businessId || global.configs.collectBusinessId,
+    theme: req.query.theme || global.configs.collectTheme,
+    page: "collect",
+  });
+});
+
+module.exports = router;
